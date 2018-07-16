62ab2afbe9301c2554a738a3480fe0a76b5bb73f
Stefan Korfitz <skj@cubeio.com>
2018-07-16 13:09:13 +0200

    ...

---
+++ App.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Next_svg from './assets/img/next';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+
+  // click metode for Next_svg
+  callFun = () => {
+
+    alert("Image Clicked!!!");
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        {/* rendering assets->img->next.js */}
+        <TouchableOpacity activeOpacity = { .7 } onPress={ this.callFun }>
+          <Next_svg/>
+        </TouchableOpacity>
+
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# demo_rn_svg
+# demo_rn_svg (https://www.npmjs.com/package/react-native-svg)
+
+Formålet med dette project, er at få afdækket hvordan man kan bruge svg'er i et react-native projekt. Det er derfor udelukkende tiltænkt som et selvstudie!...
+
+## For at bruge SVG'er i react-native, skal følgende gøres:
+
+1) installer følgende depency :
+    - "react-native-svg --save" (enten med npm install eller yarn add)
+
+2) kør kommandoen :
+    - "react-native link react-native-svg"
+
+## Dernæst:
